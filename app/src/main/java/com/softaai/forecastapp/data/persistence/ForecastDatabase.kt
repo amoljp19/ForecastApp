fb84e478079c175556b5273a6f02d59423380c2b
amol pawar <amoljp19@gmail.com>
2021-05-16 16:58:11 +0530

    added room database class

---
+++ app/src/main/java/com/softaai/forecastapp/data/persistence/ForecastDatabase.kt
@@ -0,0 +1,42 @@
+package com.softaai.forecastapp.data.persistence
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.softaai.forecastapp.model.fivedays.FiveDaysForecastApiResponse
+import com.softaai.forecastapp.model.todays.TodaysForecastApiResponse
+
+
+@Database(entities = [TodaysForecastApiResponse::class, FiveDaysForecastApiResponse::class], version = 1, exportSchema = false)
+abstract class ForecastDatabase : RoomDatabase() {
+
+    abstract fun getTodaysForecastDao(): TodaysForecastDao
+
+    abstract fun getFiveDaysForecastDao(): FiveDaysForecastDao
+
+    companion object {
+        const val DB_NAME = "forecast_database"
+
+        @Volatile
+        private var INSTANCE: ForecastDatabase? = null
+
+        fun getInstance(context: Context): ForecastDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ForecastDatabase::class.java,
+                    DB_NAME
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
